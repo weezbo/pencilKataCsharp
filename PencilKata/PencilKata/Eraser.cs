fb7ecb4a3e08fb7f6cea5f4387bf76fb75950007
Max Muir <mmuir@pillartechnology.com>
2019-02-13 12:15:31 -0500

    Added erase functionality

--- PencilKata/PencilKata/Eraser.cs
+++ PencilKata/PencilKata/Eraser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.Tracing;
 
 namespace PencilKata
@@ -11,7 +12,25 @@ namespace PencilKata
 
         public void Erase(string word, Paper paper)
         {
-            
+            int startPoint = Finder(word, paper);
+            if (startPoint > -1)
+            {
+                string frontPart = paper.Text.Substring(0, startPoint);
+                string backPart = paper.Text.Substring(startPoint);
+                string updated = backPart.Replace(word, generateSpaces(word));
+                paper.Text = frontPart + updated;
+            }
+        }
+
+        public string generateSpaces(string word)
+        {
+            string spaces = null;
+            for (int i = 0; i < word.Length; i++)
+            {
+                spaces += " ";
+            }
+
+            return spaces;
         }
     }
 }
