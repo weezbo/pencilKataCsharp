fa2ab9e3b23d01600f6db65051fb7d9fe847ee7d
Max Muir <mmuir@pillartechnology.com>
2019-02-13 09:54:07 -0500

    Base for Pencil Kata Solution

---
+++ PencilKata/PencilKata/Pencil.cs
@@ -0,0 +1,7 @@
+namespace PencilKata
+{
+    public class Pencil
+    {
+        
+    }
+}
---
+++ PencilKata/PencilKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PencilKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
